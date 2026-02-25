c8e0e2723539549391d812361980115f6932bd35
Diurno <Diurno@pc0303>
2026-02-25 09:37:44 +0100

    Codigo_Base

--- src/ies/Pureba_1/P_Prueba1.java
+++ src/ies/Pureba_1/P_Prueba1.java
@@ -3,8 +3,31 @@ package ies.Pureba_1;
 public class P_Prueba1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double a = 15.5;
+        double b = 4.0;
+
+        
+        System.out.println("--- Resultados de la Calculadora ---");
+        System.out.println("Suma: " + a + " + " + b + " = " + sumar(a, b));
+        System.out.println("Resta: " + a + " - " + b + " = " + restar(a, b));
+        System.out.println("Multiplicación: " + a + " * " + b + " = " + multiplicar(a, b));
+    }
+
+    
+    public static double sumar(double n1, double n2) {
+        return n1 + n2;
+    }
+
+    
+    public static double restar(double n1, double n2) {
+        return n1 - n2;
+    }
+
+    
+    public static double multiplicar(double n1, double n2) {
+        return n1 * n2;
+    }
 
 	}
 
-}
+
