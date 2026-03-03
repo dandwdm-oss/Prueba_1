a047a6bfa1771374df977ce5a6f9e142942cf9c5
Diurno <Diurno@pc0303>
2026-03-03 14:00:36 +0100

    fusion final

--- src/ies/Pureba_1/Proyecto_definitivo.java
+++ src/ies/Pureba_1/Proyecto_definitivo.java
@@ -5,8 +5,6 @@ public class Proyecto_definitivo {
 	public static double dividir(double n1, double n2) {
     	return n1 / n2;
     }
-
-    
     
     public static double media(double n1 , double n2) {
     	return(n1+n2)/2;}
